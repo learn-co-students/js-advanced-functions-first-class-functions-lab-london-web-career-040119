dace6e2849bbb61e1a32f010fde75b4a7ee40402
Dwake5 <wakeling101@hotmail.com>
2019-05-22 15:31:17 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+// Return first two drivers
+const returnFirstTwoDrivers = drivers => {
+    return drivers.slice(0, 2);
+};
+
+// Return last two drivers
+const returnLastTwoDrivers = drivers => {
+    return drivers.slice(-2);
+};
+
+// An array of the above two
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+// Make a higher order function that multiples a lower one
+const createFareMultiplier = mult => {
+    return cost => {
+        return cost * mult
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, whichDrivers) => {
+    return whichDrivers(drivers);
+};
